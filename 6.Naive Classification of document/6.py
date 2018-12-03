90a95ea92553b243ba73c084d96e0540af5f7f51
bietml <45572045+bietml@users.noreply.github.com>
2018-12-03 23:40:06 +0530

    Add files via upload

---
+++ 6.Naive Classification of document/6.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+
+
+msg = pd.read_csv('naive.csv', names=['message', 'label'])
+msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
+
+X = msg.message
+y = msg.labelnum
+
+xtrain, xtest, ytrain, ytest = train_test_split(X, y)
+
+count_vect = CountVectorizer()
+xtrain_dtm = count_vect.fit_transform(xtrain)
+xtest_dtm = count_vect.transform(xtest)
+
+
+print(count_vect.get_feature_names())
+
+clf = MultinomialNB().fit(xtrain_dtm, ytrain)
+predicted = clf.predict(xtest_dtm)
+
+print('Accuracy metrics')
+print('Accuracy of the classifer is', metrics.accuracy_score(ytest, predicted))
+print('Confusion matrix')
+print(metrics.confusion_matrix(ytest, predicted))
+
+print('Recall and Precison ')
+print(metrics.recall_score(ytest, predicted))
+print(metrics.precision_score(ytest, predicted))
